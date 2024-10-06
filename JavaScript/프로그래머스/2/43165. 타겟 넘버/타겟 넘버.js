9f58973818fbe37a7c5c49f9abb870eea1464bf8
ParkBible <87624953+ParkBible@users.noreply.github.com>
2024-10-07 00:26:55 +0900

    [level 2] Title: 타겟 넘버, Time: 16.87 ms, Memory: 35.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,18 @@
 function solution(numbers, target) {
-    nums = numbers;
     count = 0;
     
-    dfs(target, 0, 0);
-    return count;
-}
-
-const dfs = (target, lv, val) => {   
-    if (lv === nums.length) {
-        if (val === target) {
-            count++;
+    const dfs = (lv, val) => {
+        if (lv === numbers.length) {
+            if (val === target) {
+                count++;
+            }
+        } else {
+            dfs(lv + 1, val + numbers[lv]);
+            dfs(lv + 1, val - numbers[lv]);
         }
-    } else {
-        dfs(target, lv + 1, val + nums[lv]);
-        dfs(target, lv + 1, val - nums[lv]);
     }
+    
+    dfs(0, 0);
+    
+    return count;
 }
